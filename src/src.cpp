73caf159c96997cdd3290c24ceb1e5407574a865
Yo-yuya <soroguzugosa@gmail.com>
2020-06-14 00:40:49 +0900

    enumを利用したパターン

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,50 @@
-﻿#include <iostream>
-#include <thread>
+﻿#include <stdio.h>
+#include <iostream>
+
+enum State_base {
+    STATE_CREDIT,
+    STATE_TITLE,
+    STATE_OPENING,
+    STATE_DEMO
+};
 
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+    State_base now = STATE_CREDIT;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+    while (true)
+    {
+        switch (now)
+        {
+        case STATE_CREDIT:
+            printf("credit\n");
+            std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+            now = STATE_TITLE;
+            
+            break;
+            
+        case STATE_TITLE:
+            printf("title\n");
+            std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+            now = STATE_OPENING;
+            break;
 
-    std::cout << "title\n";
+        case STATE_OPENING:
+            printf("opning\n");
+            std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+            now = STATE_DEMO;
+            break;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        case STATE_DEMO:
+            printf("demo\n");
+            std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+            now = STATE_CREDIT;
+            break;
+            
+             
+ 
 
-    std::cout << "opening\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
-    std::cout << "demo\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+        }
+    }
     
-    goto repeat;
 }
-
